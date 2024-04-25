2df5547421f200c9b164911952ad8590579b1ed4
Docente Tecnologico Comfenalco <docente@Academico.local>
2024-04-24 19:53:03 -0500

    Capa entidad

---
+++ src/FlatUI.Solution/Capa.Entidad/Usuario.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Capa.Entidad
+{
+    public class Usuario
+    {
+		public int IdUsuario { get; set; }
+		public string NombreUsuario { get; set; }
+		public string NombreCompleto { get; set; }
+		public string Correo { get; set; }
+		public string Clave { get; set; }
+		public bool Estado { get; set; }
+		public DateTime FechaRegistro { get; set; }
+    }
+}
